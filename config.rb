196f06d7c83ec2dd5b47650976211d193aa3144e
parthac98 <thisispartha@gmail.com>
2016-08-04 14:40:53 +0800

    Adding files

---
+++ config.rb
@@ -0,0 +1,11 @@
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "sass"
+css_dir = "css"
+images_die = "img"
+fonts_dir = "fonts"
+javascript_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
